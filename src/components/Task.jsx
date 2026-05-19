9e4560157f17bd55e04834f4ee081e517468a07e
kamaukimani <paulkimys285@gmail.com>
2026-05-19 19:33:30 +0300

    starter code for Task component

---
+++ src/components/Task.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Task(){
+    return(
+        <div>
+            <div>Category</div>
+            <div>Text</div>
+            <button>X</button>
+        </div>
+    )
+}
+export default Task;

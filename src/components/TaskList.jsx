d78801767cb614228b1a2bc9572289b764e52c38
kamaukimani <paulkimys285@gmail.com>
2026-05-19 20:13:23 +0300

    passed down text and category as props to Task component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Task from './Task';
 
-function TaskList(){
+function TaskList({tasks}){
+    //console.log(tasks)
+    const oldTasks=tasks.map((task,index)=>(
+        <Task key={index} text={task.text} category={task.category} />
+    ))
     return(
         <div>
-            <Task />
+            {oldTasks}
         </div>
     )
 }

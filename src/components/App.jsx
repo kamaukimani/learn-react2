c4ff861dfec73bfd59b96bc28fa4e54b9a1ab84a
kamaukimani <paulkimys285@gmail.com>
2026-05-19 20:02:43 +0300

    render Header component as well as pass props to it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {CATEGORIES,TASKS} from '../data'
 import TaskList from './TaskList';
 import NewTaskForm from './NewTaskForm';
 import CategoryFilter from './CategoryFilter';
+import Header from './Header';
 function App(){
+    const [isDark,setIsDark]=useState(false);
+    function handleDarkMode(){
+        setIsDark(isDark=>!isDark)
+    }
+    const color=isDark ? "black" : "white"
     return(
-        <div>
+        <div style={{background:color}}>
+            <Header isDark={isDark} handleDarkMode={handleDarkMode}/>
             <h2>My Tasks</h2>
             <CategoryFilter />
             <NewTaskForm />

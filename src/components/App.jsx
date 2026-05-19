9aef8eb7cc7ce8e4f024a6005bbf20f3617d46da
kamaukimani <paulkimys285@gmail.com>
2026-05-19 18:22:41 +0300

    check if the remote repo is connected to the local repo in github

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App(){
     return(
-        <div>Hello....welcome to this React Application</div>
+        <div>
+            Hello....welcome to this React Application
+            Is the push working 
+        </div>
     )
 }
 export default App;

419efda460379490d1580144a76998a71a169214
kamaukimani <paulkimys285@gmail.com>
2026-05-19 19:45:57 +0300

    starter code for CategoryFilter component

---
+++ src/components/CategoryFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CategoryFilter(){
+    return(
+        <div>
+            <h5>Category Filters</h5>
+            {/*render <button> element for each categoy here */}
+        </div>
+    )
+}
+export default CategoryFilter;

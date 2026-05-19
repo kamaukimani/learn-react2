ed95f3f95d724f7f05ae2b9ac3d6c6ef40fab17c
kamaukimani <paulkimys285@gmail.com>
2026-05-19 19:54:16 +0300

    import and render components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import {CATEGORIES,TASKS} from '../data'
+import TaskList from './TaskList';
+import NewTaskForm from './NewTaskForm';
+import CategoryFilter from './CategoryFilter';
 function App(){
     return(
         <div>
-            Hello....welcome to this React Application
-            Is the push working 
-            okay
+            <h2>My Tasks</h2>
+            <CategoryFilter />
+            <NewTaskForm />
+            <TaskList />
         </div>
     )
 }

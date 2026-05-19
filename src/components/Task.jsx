9967047e0c633bc39b48c97eee1078bea29762e2
kamaukimani <paulkimys285@gmail.com>
2026-05-19 20:14:14 +0300

    render category and text to the UI

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function Task(){
+function Task(props){
+    const {text,category}=props
     return(
         <div>
-            <div>Category</div>
-            <div>Text</div>
+            <div>{category}</div>
+            <div>{text}</div>
             <button>X</button>
         </div>
     )

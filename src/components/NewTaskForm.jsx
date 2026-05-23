38643c35a4445d6b0a1288461dbe3cf4025f965d
kamaukimani <paulkimys285@gmail.com>
2026-05-23 19:04:08 +0300

    add the submit and addNewTask logic

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function NewTaskForm(){
+function NewTaskForm({categories,isTask,setIsTask}){
+     const initialFormData={
+        text:"",
+        category:"Code"
+    }
+    const [formData,setFormData]=useState(initialFormData)
+   
+    function handleSubmit(event){
+        event.preventDefault()
+        const newItem={
+            text:formData.text,
+            category:formData.category
+        }
+        setIsTask([...isTask,newItem])
+        setFormData(initialFormData)
+    }
+    function handleAdd(event){
+        const name=event.target.name;
+        const value=event.target.value;
+        setFormData({
+            ...formData,
+            [name]:value
+        })
+    }
+   
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Details
-                <input type="text" name="text" />
+                <input type="text" name="text" value={formData.text} onChange={handleAdd}/>
             </label>
             <label>
                 Category
-                <select>
-
+                <select name="category" value={formData.category} onChange={handleAdd}>
+                    <option value={categories[1]}>Code</option>
+                    <option value={categories[2]}>Food</option>
+                    <option value={categories[3]}>Money</option>
+                    <option value={categories[4]}>Misc</option>
                 </select>
             </label>
             <input type="submit" value="Add Task" />

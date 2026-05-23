f4093c4321b9df227abc5c4c21533499386cfc09
kamaukimani <paulkimys285@gmail.com>
2026-05-23 19:05:36 +0300

    add filter logic to render filtered categories

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function CategoryFilter(){
+function CategoryFilter({categories,setFilterBy}){
+    //console.log(isTask);
+  
+    const category=categories.map((cat,index)=>(
+        <button key={index} onClick={()=>setFilterBy(cat)}>{cat}</button>
+    ))
+
+    
     return(
         <div>
             <h5>Category Filters</h5>
             {/*render <button> element for each categoy here */}
+            {category}
         </div>
     )
 }

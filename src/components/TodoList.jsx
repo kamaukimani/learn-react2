3b4876a423e46f7319943a34aebdbbb4a289f05b
kamaukimani <paulkimys285@gmail.com>
2026-05-24 19:19:25 +0300

    starter code for TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TodoList(){
+    return(
+        <div>
+            <h2>My Todos</h2>
+            <ul>
+                <TodoItem/>
+            </ul>
+        </div>
+    )
+}
+export default TodoList;
